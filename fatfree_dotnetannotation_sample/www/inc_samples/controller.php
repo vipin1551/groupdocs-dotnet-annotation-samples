7401317fb4a017d694949f1b4b1adf3555a1089e
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2013-12-19 09:07:30 +0200

    updated user autorisation and added document selection

--- fatfree_dotnetannotation_sample/www/inc_samples/controller.php
+++ fatfree_dotnetannotation_sample/www/inc_samples/controller.php
@@ -5,6 +5,7 @@ F3::set('handler', '');
 $url = F3::get('POST["url"]');
 $handler = F3::get('POST["handler"]');
 $userName = F3::get('POST["userName"]');
+$documentName = F3::get('POST["documentName"]');
 //Check is URL entered
 if (empty($url)) {
     $error = 'Please enter URL of installed GroupDocs .NET Annotation';
@@ -33,10 +34,13 @@ if (empty($url)) {
     if ($userName == "") {
         $userName = "Anonymous";
     }
+    //Remove spaces and tags from document name for view and annotate
+    $documentName = trim(strip_tags($documentName));
     //Set variables for template
     F3::set("url", $url);
     F3::set("userName", $userName);
     F3::set("handler", $handler);
+    F3::set("documentName", $documentName);
 }
 //Process template
 echo Template::serve('view.htm');
